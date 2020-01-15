6daa8ac4ccf0ea7bdd342bb0356bcdb5eeb7792d
Max <pandaizzda.business@gmail.com>
2020-01-15 14:21:30 +0100

    First Commit

---
+++ IdeaProjects/ÜbungsAufgaben/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        while(true) {
+            int number = 1 + (int)(Math.random() * 6);
+            counter++;
+            if(number == 6) {
+                break;
+            }
+        }
+        System.out.println("Es wurden " + counter + " Versuche benötigt.");
+    }
+}
